2ffdb6e77a2024e0b0ba798a67b75fb995c6f93e
deadman2496 <a.marroquin2496@gmail.com>
2024-02-12 22:05:12 -0500

    added search bar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -13,7 +13,7 @@ export const MobileSidebar = () => {
             <SheetTrigger className="md:hidden pr-4">
                 <Menu />
             </SheetTrigger>
-            <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+            <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">0
                 <Sidebar />
             </SheetContent>
         </Sheet>
